ab3a9e88485c532e43143ce314ee981cb90518aa
Xxiong <xxiong@cqu.edu.cn>
2017-12-08 01:15:50 -0500

    syntax test for haskell

--- haskell/syntax-test/helloworld.hs
+++ haskell/syntax-test/helloworld.hs
@@ -1,9 +1,38 @@
+--main = putStrLn "Hello, World!"
+
 {-
-main = putStrLn "Hello, World!"
--}
-
-
 main = do
     putStrLn "Hello, what's your name?"
     name <- getLine
     putStrLn ("Hey " ++ name ++ ", you rock!")
+-}
+
+{-
+main = do
+    putStrLn "Hello, what's your name?"
+    name <- getLine
+    putStrLn $ "Read this carefully, because this is your future: " ++ name
+-}
+
+
+doubleMe x = x + x
+
+
+doubleUs x y = doubleMe x + doubleMe y
+
+
+doubleSmallNumber x = if x > 100 
+    then x
+    else x * 2
+
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+
+gnekiah'Hsiung = "My name is Gnekiah Hsiung!"
+
+
+length' xs = sum [1 | _ <- xs]
+
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
